5646f3b1a9d9f2af79b6725455e4f17bb12c38d5
kduane <karl.duane@gmail.com>
2018-08-09 18:51:30 -0400

    updated app.rb to use ActiveRecord

--- app.rb
+++ app.rb
@@ -7,22 +7,23 @@ also_reload 'models/contact'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 
-before do
-  contact_attributes = [
-    { first_name: 'Eric', last_name: 'Kelly', phone_number: '1234567890' },
-    { first_name: 'Adam', last_name: 'Sheehan', phone_number: '1234567890' },
-    { first_name: 'Dan', last_name: 'Pickett', phone_number: '1234567890' },
-    { first_name: 'Evan', last_name: 'Charles', phone_number: '1234567890' },
-    { first_name: 'Faizaan', last_name: 'Shamsi', phone_number: '1234567890' },
-    { first_name: 'Helen', last_name: 'Hood', phone_number: '1234567890' },
-    { first_name: 'Corinne', last_name: 'Babel', phone_number: '1234567890' }
-  ]
-
-  @contacts = contact_attributes.map do |attr|
-    Contact.new(attr)
-  end
-end
+# before do
+#   contact_attributes = [
+#     { first_name: 'Eric', last_name: 'Kelly', phone_number: '1234567890' },
+#     { first_name: 'Adam', last_name: 'Sheehan', phone_number: '1234567890' },
+#     { first_name: 'Dan', last_name: 'Pickett', phone_number: '1234567890' },
+#     { first_name: 'Evan', last_name: 'Charles', phone_number: '1234567890' },
+#     { first_name: 'Faizaan', last_name: 'Shamsi', phone_number: '1234567890' },
+#     { first_name: 'Helen', last_name: 'Hood', phone_number: '1234567890' },
+#     { first_name: 'Corinne', last_name: 'Babel', phone_number: '1234567890' }
+#   ]
+#
+#   @contacts = contact_attributes.map do |attr|
+#     Contact.new(attr)
+#   end
+# end
 
 get '/' do
+  @contacts = Contact.all
   erb :index
 end
